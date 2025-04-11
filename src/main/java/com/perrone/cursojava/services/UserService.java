83b57e14976d0a5dd5370ccdf179ab81e2cfdf8f
Danilo <danilo.perrone@gmail.com>
2025-04-11 16:33:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/perrone/cursojava/services/UserService.java
@@ -0,0 +1,27 @@
+package com.perrone.cursojava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.perrone.cursojava.entities.User;
+import com.perrone.cursojava.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
