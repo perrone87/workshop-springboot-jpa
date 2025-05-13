2bc877ae2b5b62b0fed0c2c5f4860e2fb75dc582
Danilo <danilo.perrone@gmail.com>
2025-05-13 14:45:05 -0300

    Payment, one-to-one association

--- src/main/java/com/perrone/cursojava/config/TestConfig.java
+++ src/main/java/com/perrone/cursojava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.perrone.cursojava.entities.Category;
 import com.perrone.cursojava.entities.Order;
 import com.perrone.cursojava.entities.OrderItem;
+import com.perrone.cursojava.entities.Payment;
 import com.perrone.cursojava.entities.Product;
 import com.perrone.cursojava.entities.User;
 import com.perrone.cursojava.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
