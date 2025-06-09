a90dab464c4288dad3856e019ac8991cd4b5bb4d
Danilo <danilo.perrone@gmail.com>
2025-06-09 15:42:24 -0300

    Exception handling - update

--- src/main/java/com/perrone/cursojava/services/UserService.java
+++ src/main/java/com/perrone/cursojava/services/UserService.java
@@ -12,6 +12,8 @@ import com.perrone.cursojava.repositories.UserRepository;
 import com.perrone.cursojava.services.exceptions.DatabaseException;
 import com.perrone.cursojava.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
